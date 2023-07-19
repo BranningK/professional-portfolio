fd180e39ab39c30b7adff089b4ab51dcdc45ba3c
Branning <bmk2110@gmail.com>
2023-07-19 16:36:26 -0400

    changed carousel to jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from './components/Navbar';
 import { Banner } from './components/Banner';
-import { Projects } from './components/Projects';
-import { Footer } from './components/Footer';
-import { Carousel } from 'react-bootstrap';
+// import { Projects } from './components/Projects';
+// import { Footer } from './components/Footer';
+import { Carousel } from './components/Carousel';
 
 
 function App() {
