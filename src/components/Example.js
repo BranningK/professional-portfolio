de18a292cf8be4e5eff73199c9274e8573d442a1
Branning <bmk2110@gmail.com>
2023-08-01 16:54:51 -0400

    added project titles and some images

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import Carousel from 'react-elastic-carousel';
 import githubDark from '../assets/logos/github/githubDark.png';
+import weatherApp from '../assets/weatherPostSearch.png';
 
 const breakpoints = [
   { width : 1, itemsToShow: 1 },
@@ -12,12 +13,12 @@ const breakpoints = [
 export default class App extends Component {
   state ={
     items: [
-      { id: 1, img: githubDark, alt: "Blah" , title: `item 1` },
-      { id: 1, img: githubDark, alt: "Blah" , title: `item 1` },
-      { id: 1, img: githubDark, alt: "Blah" , title: `item 1` },
-      { id: 1, img: githubDark, alt: "Blah" , title: `item 1` },
-      { id: 1, img: githubDark, alt: "Blah" , title: `item 1` },
-      { id: 1, img: githubDark, alt: "Blah" , title: `item 1` },
+      { id: 1, img: githubDark, alt: "Image of the project running in the terminal" , title: `README Generator` },
+      { id: 2, img: weatherApp, alt: "Blah" , title: `Weather Forecast` },
+      { id: 3, img: githubDark, alt: "Blah" , title: `Random Password Generator` },
+      { id: 4, img: githubDark, alt: "Blah" , title: `Team Profile Generator` },
+      { id: 5, img: githubDark, alt: "Blah" , title: `MusiQuest` },
+      { id: 6, img: githubDark, alt: "Blah" , title: `Workplace Tracker` },
     ]
   }
 
@@ -25,7 +26,7 @@ export default class App extends Component {
     const { items } = this.state;
     return (
       <Carousel breakPoints={breakpoints}>
-        {items.map(item => <img className="carouselProject" key={item.id} src={item.img} alt={item.alt}/>)}
+        {items.map(item => <div className="carouselProject"><img className="carouselImage" key={item.id} src={item.img} alt={item.alt}/><p className="carouselText">{item.title}</p></div>)}
       </Carousel>
     )
   }
